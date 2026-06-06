a188295a7ecdbddf689eeafdd2ee521a2fc8952c
ynnning <ynnning@gmail.com>
2026-06-06 13:58:57 +0000

    add MMLU_2CAT signal of life experiment config and dataset

--- .ipynb_checkpoints/pipeline-checkpoint.py
+++ .ipynb_checkpoints/pipeline-checkpoint.py
@@ -43,6 +43,7 @@ class Datasets(Enum):
     YEARS = auto()
     YEARS_TF = auto()
     MMLU = auto()
+    MMLU_2CAT = auto()
     WMDP_CORPUS = auto()
     WMDP_CORPUS_FINEWEB = auto()
     WMDP_CORPUS_MMLU = auto()
@@ -735,6 +736,34 @@ datasets_dict = {
         "dev_file": "mmlu_cats_random_trimmed/dev",
         "retain_dev_file": "mmlu_cats_random_trimmed/dev",
     },
+    Datasets.MMLU_2CAT: {
+        "unlearn_files": [
+            f"mmlu_cats_random_trimmed/corpus_mmlu_{mmlu_cats_forget[i]}"
+            for i in range(2)
+        ],
+        "wrong_unlearn_files": [
+            f"mmlu_cats_random_trimmed/whp_corpus_mmlu_{mmlu_cats_forget[i]}"
+            for i in range(2)
+        ],
+        "fixed_wrong_unlearn_files": [
+            f"mmlu_cats_random_trimmed/fwf_corpus_mmlu_{mmlu_cats_forget[i]}"
+            for i in range(2)
+        ],
+        "val_files": [
+            f"mmlu_cats_random_trimmed/mmlu_{mmlu_cats_forget[i]}"
+            for i in range(2)
+        ],
+        "retain_files": [
+            f"mmlu_cats_random_trimmed/corpus_mmlu_{mmlu_cats_retain[i]}"
+            for i in range(2)
+        ],
+        "val_retain_files": [
+            f"mmlu_cats_random_trimmed/mmlu_{mmlu_cats_retain[i]}"
+            for i in range(2)
+        ],
+        "dev_file": "mmlu_cats_random_trimmed/dev",
+        "retain_dev_file": "mmlu_cats_random_trimmed/dev",
+    },
     Datasets.WMDP_CORPUS: {
         "unlearn_files": [
             f"wmdp/bio-forget-coprus",
@@ -1041,7 +1070,7 @@ def get_num_gpus():
         return 0
 
 
-config_file = "gd_ent"
+config_file = "gd_ent_2cat"
 
 # The main function that reads configurations from hydra config files and calls
 # `main()` for each unlearning configuration
