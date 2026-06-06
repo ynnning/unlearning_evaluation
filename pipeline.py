52860b3af5fe3dcee5e5523987645b109c255bb2
ynnning <ynnning@gmail.com>
2026-06-06 17:11:00 +0000

    stem/cul vs cul/social

--- pipeline.py
+++ pipeline.py
@@ -623,8 +623,11 @@ def main(
 
 # MMLU categories to use for forget loss
 
-mmlu_cats_forget = ["STEM", "chemistry", "business", "health", "geography"]
-mmlu_cats_retain = ["social sciences", "culture", "history", "law", "philosophy",]
+#mmlu_cats_forget = ["STEM", "chemistry", "business", "health", "geography"]
+#mmlu_cats_retain = ["social sciences", "culture", "history", "law", "philosophy",]
+
+mmlu_cats_forget = ["STEM", "culture", "business", "health", "geography"]
+mmlu_cats_retain = ["social sciences", "chemistry", "history", "law", "philosophy",]
 
 #mmlu_cats_forget = ["STEM", "business", "chemistry", "culture", "geography"]
 #mmlu_cats_retain = ["health", "history", "law", "philosophy", "social sciences"]
